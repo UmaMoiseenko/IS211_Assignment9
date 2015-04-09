c87a0b98a634a832e7a66ca6382ba1770a6f8b39
Uma <uma.moiseenko@gmail.com>
2015-04-08 23:46:48 -0400

    small update

--- apple_stock.py
+++ apple_stock.py
@@ -5,17 +5,16 @@ soup = BeautifulSoup (urlopen("http://finance.yahoo.com/q/hp?s=AAPL+Historical+P
 dates = soup.findAll('table', attrs={'class': 'yfnc_datamodoutline1'})[0].findAll('table')[0].findAll('tr')
 counter = 0
 
-
 for date in dates:
 	counter +=1
 
-	date_length = dates[counter].findAll('td')
+	date = dates[counter].findAll('td')
 
-	if len(date_length) > 1:
-		print dates[counter].findAll('td', attrs={'class': 'yfnc_tabledata1', 'align': 'right'})[0].contents[0]
+	if len(date) > 1:
+		print date[0].contents[0]
 
-		if len(date_length) > 2:
-			print ' Close Price: ', dates[counter].findAll('td', attrs={'class': 'yfnc_tabledata1', 'align': 'right'})[4].contents[0]
+		if len(date) > 2:
+			print ' Close Price: ', date[4].contents[0]
 
 	else:
 		break
